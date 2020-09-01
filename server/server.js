1c363922ca54eb7e4ded292dd9f6d5f4146b2c13
Damien Brockmann <damienbrockmann@gmail.com>
2020-09-01 12:43:13 -0500

    server dockerfile created

--- server/server.js
+++ server/server.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const bodyParser = require('body-parser');
 
+const routes = require('./routes/studentRoutes');
+require ('./db');
+
+const port = process.env.PORT || 8080;
 app.listen(port);
-console.log('todo list RESTful API server started on: ' + port);
+
+console.log('API server started on: ' + port);
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+routes(app);
