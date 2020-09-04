8fc5aacccf53e12c8c57f52baef8faa9dac4d6ef
Damien Brockmann <damienbrockmann@gmail.com>
2020-09-04 10:03:30 -0500

    pagination keys and limit added to listall functionality

--- server/controllers/studentController.js
+++ server/controllers/studentController.js
@@ -6,15 +6,35 @@ const handleError = (res, err) => {
   res.status(500).send(err);
 }
 
+const createFilters = (filternames, req) => {
+  // filters exist if there is at least one in the query string
+  const filtersExist = filternames.reduce((result, name) => {
+    return result || !!req.query[name]
+  }, false);
+  // if there are no filters in query, return false
+  if (!filtersExist) return false;
+  
+  // iterate over filternames to create an object, e.g. { firstname: req.query.firstname }
+  return filternames.reduce((obj, name) => {
+    return {...obj, [name]: req.query[name] }
+  }, {})
+}
+
 exports.list = (req, res) => {
   try {
-    const filters = req.query.firstname || req.query.lastname ? { firstname: req.query.firstname, lastname: req.query.lastname } : false;
+    const filters = createFilters(['firstname', 'lastname'], req);
+    
     listAllCount((counterr, rows) => {
       if (counterr) return handleError(res, counterr);
+      
+      const allfilters = createFilters(['firstname', 'lastname', 'firstnamekey', 'lastnamekey', 'idkey'], req);
+      const limit = req.query.limit;
+
       listAll((err, results) => {
         if (err) return handleError(res, err);
         res.json({results, count: rows[0].total });
-      }, filters);
+      }, allfilters, limit);
+
     }, filters);
   } catch (e) {
     handleError(res, e);
